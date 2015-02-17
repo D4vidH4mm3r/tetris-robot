482da9478c37e7f76e590cf10180ef7fcb86b25c
David Hammer <d4vid.h4mm3r@gmail.com>
2015-02-17 18:50:09 +0100

    Mere Makefile og objekter

--- Board.h
+++ Board.h
@@ -1,5 +1,6 @@
 #ifndef _board_h
 #define _board_h
+
 #include "common.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,105 +12,4 @@ void board_destroy(int **board);
 int board_collapse(int **board);
 int board_dead(int **board);
 
-int **board_create() {
-	int **b = malloc(BOARD_HEIGHT*sizeof(int *));
-	for (int row=0; row<BOARD_HEIGHT; row++) {
-		b[row] = malloc(BOARD_WIDTH*sizeof(int));
-		for (int col=0; col<BOARD_WIDTH; col++) {
-			b[row][col] = 0;
-		}
-	}
-	return b;
-}
-
-void board_copy(int **copy, int **board) {
-	for (int row=0; row<BOARD_HEIGHT; row++) {
-		for (int col=0; col<BOARD_WIDTH; col++) {
-			copy[row][col] = board[row][col];
-		}
-	}
-}
-
-void board_print(int **b) {
-	for (int i=0; i<BOARD_WIDTH+2; i++) {
-		printf("-");
-	}
-	printf("\n");
-	for (int row=0; row<BOARD_HEIGHT; row++) {
-		printf("|");
-		for (int col=0; col<BOARD_WIDTH; col++) {
-			if (b[row][col]) {
-				switch (b[row][col]) {
-					case BLUE:
-						printf("\x1B[34m");
-						break;
-					case TEAL:
-						printf("\x1B[36m");
-						break;
-					case YELLOW:
-						printf("\x1B[33m");
-						break;
-					case MAGENTA:
-						printf("\x1B[35m");
-						break;
-					case GREEN:
-						printf("\x1B[32m");
-						break;
-					case RED:
-						printf("\x1B[31m");
-						break;
-					case WHITE:
-						printf("\x1B[37m");
-						break;
-				}
-				printf("#\x1B[0m");
-			} else {
-				printf(" ");
-			}
-		}
-		printf("|\n");
-	}
-	for (int i=0; i<BOARD_WIDTH+2; i++) {
-		printf("-");
-	}
-	printf("\n");
-}
-
-int board_collapse(int **board) {
-	int cleared = 0;
-	for (int row=BOARD_HEIGHT-1; row>=0; row--) {
-		int full=1;
-		for (int col=0; col<BOARD_WIDTH; col++) {
-			if (!board[row][col]) {
-				full = 0;
-			}
-		}
-		if (full) {
-			cleared++;
-			free(board[row]);
-			for (int fall=row; fall>0; fall--) {
-				board[fall] = board[fall-1];
-			}
-			board[0] = calloc(BOARD_WIDTH,sizeof(int));
-			row++;
-		}
-	}
-	return cleared;
-}
-
-int board_dead(int **board) {
-	for (int col=0; col<BOARD_WIDTH; col++) {
-		if (board[0][col]) {
-			return 1;
-		}
-	}
-	return 0;
-}
-
-void board_destroy(int **board) {
-	for (int row=0; row<BOARD_HEIGHT; row++) {
-		free(board[row]);
-	}
-	free(board);
-}
 #endif
--- Makefile
+++ Makefile
@@ -1,8 +1,17 @@
-LDFLAGS=-L/usr/include -lX11 
-CFLAGS=-std=c99 
+LDFLAGS = -L/usr/include -lX11
+CFLAGS = -std=c99
+OBJS = Block.o AI.o Board.o
 CC=gcc
 
 all: main
 
-grab: grab.c
-	$(CC) $(CFLAGS) grab.c -o grab $(LDFLAGS)
+main: main.c $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) main.c -o main
+
+grab: grab.c Board.o
+	$(CC) $(CFLAGS) Board.o grab.c -o grab $(LDFLAGS)
+
+clean:
+	rm -f *.o
+	rm -f grab
+	rm -f main
