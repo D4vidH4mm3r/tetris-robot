c34ff7742d8aa7aa8b660b321a802b9e69b3c32d
David Hammer <d4vid.h4mm3r@gmail.com>
2015-02-18 09:12:22 +0100

    Sætter op til at måle perf

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 LDFLAGS = -L/usr/include -lX11 -lXtst
-CFLAGS = -std=c99
+CFLAGS = -std=c99 #-Ofast
 OBJS = Block.o AI.o Board.o
 CC=gcc
 
 all: bin/ai_demo bin/play
 
-bin/ai_demo: ai_demo.c $(OBJS)
+bin/ai_demo: ai_demo.c $(OBJS) common.h
 	$(CC) $(CFLAGS) $(OBJS) ai_demo.c -o bin/ai_demo
 
-bin/play: play.c $(OBJS)
+bin/play: play.c $(OBJS) common.h
 	$(CC) $(CFLAGS) $(OBJS) play.c -o bin/play $(LDFLAGS)
 
 clean:
--- ai_demo.c
+++ ai_demo.c
@@ -17,7 +17,7 @@ int main(void) {
 	Block *queue = malloc(2*sizeof(Block));
 	queue[0] = *blocks[random_blockno()];
 
-	while (1) {
+	for (int i=0; i<2000; i++) {
 		queue[1] = *blocks[random_blockno()];
 
 		Move *best = move_best_lookahead(b, &queue[0], &queue[1]);
@@ -28,14 +28,17 @@ int main(void) {
 
 		if (board_dead(b)) {
 			board_print(b);
+			printf("%d cleared\n", cleared);
 			printf("Dead :(\n");
-			break;
+			board_destroy(b);
+			b = board_create();
 		}
 		cleared += board_collapse(b);
 		//board_print(b);
-		printf("%d cleared\n", cleared);
 
 		queue[0] = queue[1];
 	}
+	board_print(b);
+	printf("%d cleared\n", cleared);
 	return 0;
 }
